d1bc3891a0b7397063279187d5ccdf36b009a0a4
NRG213 <Suramen323@gmail.com>
2024-06-13 22:57:36 +0200

    DoneProject

---
+++ ProjectSchool/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody rb;
+    public float speed = 10.0F;
+    float rotationSpeed = 50.0F;
+    
+    static public bool dead = false;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    
+	void FixedUpdate () {
+	
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime * 5f;
+        Quaternion turn = Quaternion.Euler(0f,rotation,0f);
+        rb.MovePosition(rb.position + this.transform.forward * translation);
+        rb.MoveRotation(rb.rotation * turn);
+    }
+}
